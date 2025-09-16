3b687608c5d24853e1eb721ce3e2bcf79a7a034e
Ibrahim Attia <hima.attia.123@gmail.com>
2025-09-15 22:32:51 -0400

    Added plan so far to solve

--- Functions.java
+++ Functions.java
@@ -7,7 +7,8 @@ public class Functions{
 In this comment block, explain what your group discussed and how you wish to partition the problem. 
 List the cases, and how finding the answer differs in each case.
 Your response:
-
+If there is not enough or exactly enough big bricks, check if there is enough small bricks to continue to reach the goal (sometimes, no small bricks will be necessary).
+If there is more than enough big bricks, check if the differential can be accounted for with small bricks.
 
  */
 
