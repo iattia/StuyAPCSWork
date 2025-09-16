ee30aa58140402dafe89ca3a92e3daecc91a4dab
Ibrahim Attia <hima.attia.123@gmail.com>
2025-09-16 17:29:21 -0400

    Added commented English solution and code implementation

--- Functions.java
+++ Functions.java
@@ -8,13 +8,15 @@ In this comment block, explain what your group discussed and how you wish to par
 List the cases, and how finding the answer differs in each case.
 Your response:
 If there is not enough or exactly enough big bricks, check if there is enough small bricks to continue to reach the goal (sometimes, no small bricks will be necessary).
+OR
 If there is more than enough big bricks, check if the differential can be accounted for with small bricks.
 
  */
 
  /*STEP 2: Write the test cases first in the driver. Pay attention to how the test function is called.*/
+ 
  /*STEP 3: Complete the function makeBricks.*/
  public static boolean makeBricks(int small, int big, int goal){
-  return false;//placeholder return value; 
+  return (goal >= 5 * big && small >= goal -5*big || goal < 5*big && small >=goal % 5);
  }
 }
