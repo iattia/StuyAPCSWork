4c9c2b982da5c554656957bea667c57b605d00b4
Ibrahim Attia <iattia70@stuy.edu>
2025-09-25 10:51:57 -0400

    Added needed test cases

--- Driver.java
+++ Driver.java
@@ -7,39 +7,42 @@ public class Driver{
   String whole;
   String part;
   String number;
-  
-  
+
+
   //test cases myCompareTo:
   a="cat";
   b="dog";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
   a="first";
   b="second";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
   a="alphabetical";
   b="zalphabetical";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
   a="prefix";
   b="prefixOf";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
   a="zalphabetical";
   b="alphabetical";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
   a="a";
   b="a";
-  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
+  System.out.println("Expected same sign: "+ a.compareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b));
 
   //test cases myIndexOf:
   whole = "Hello";
   part = "He";
   System.out.println("Expected "+ whole.indexOf(part) +" vs my function: "+ StringMethods.myIndexOf(whole, part) );
   whole = "SecondTest";
-  part = "est"; 
+  part = "est";
   System.out.println("Expected "+ whole.indexOf(part) +" vs my function: "+ StringMethods.myIndexOf(whole, part) );
   whole = "ThirdTest";
   part = "dT";
   System.out.println("Expected "+ whole.indexOf(part) +" vs my function: "+ StringMethods.myIndexOf(whole, part) );
-    
+  whole = "FourthTest";
+  part = "notInWhole";
+  System.out.println("Expected "+ whole.indexOf(part) +" vs my function: "+ StringMethods.myIndexOf(whole, part) );
+
   //test cases parseInt:
   number = "152";
   int actual = Integer.parseInt(number);
