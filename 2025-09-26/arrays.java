70e5951146cb928d0b93da19a5677e4cba86af02
Ibrahim Attia <iattia70@stuy.edu>
2025-09-26 11:01:49 -0400

    9/26/25 Classwork

---
+++ 2025-09-26/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+  public static void main(String[] args){
+    int[] anArray = new int[11];
+    System.out.println(anArray);
+    for (int i=0;i<anArray.length;i++){
+      anArray[i] = 100 + 10*i;
+    }
+    System.out.println(anArray[0]);
+    System.out.println(anArray[10]);
+    System.out.println(anArray[5]);
+    for (int i=0;i<anArray.length;i++){
+      System.out.println(anArray[i]);
+    }
+  }
+}
