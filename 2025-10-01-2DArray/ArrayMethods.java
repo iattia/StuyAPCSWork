fc9d33c662c8025954d10453d91a82644982de60
Ibrahim Attia <iattia70@stuy.edu>
2025-10-03 11:00:29 -0400

    Worked on duplicate array method, completed replace negative array method

--- 2025-10-01-2DArray/ArrayMethods.java
+++ 2025-10-01-2DArray/ArrayMethods.java
@@ -11,6 +11,7 @@ public class ArrayMethods{
     newStr += "]";
     return newStr;
   }
+
   public static String aryToString(int[][] nums){
     String newStr = "[";
     for (int i=0; i<nums.length; i++){
@@ -22,11 +23,37 @@ public class ArrayMethods{
     newStr += "]";
     return newStr;
   }
+
+  public static void replaceNegative(int[][] vals){
+    for (int i=0; i<vals.length; i++){
+      for (int j=0; j<vals[i].length; j++){
+        if (vals[i][j]<0){
+          if (i == j){
+            vals[i][j] = 1;
+          } else {
+            vals[i][j] = 0;
+          }
+        }
+      }
+    }
+  }
+
+  public static int[][] copy(int[][] nums){
+    int[][] newArray = new int[nums.length][]
+  }
+
   public static void main(String[] args){
+    int[][] newArray = new int[3][3];
     int[][] arr = new int[][]{{5,6,7},{1,2,3}};
     String result = "";
     result = aryToString(arr);
     System.out.println(result + " expected " + "[[5, 6, 7], [1, 2, 3]]");
-   
+    arr = new int[][]{{-1,0,3},{-3,-1,-5}};
+    replaceNegative(arr);
+    result = aryToString(arr);
+    System.out.println(result + " expected " + "[[1, 0, 3], [0, 1, 0]]");
+    newArray = copy(arr);
+    System.out.println(arr + " (address should be different from " +  newArray + " ||||| copied contents: " + aryToString(newArray) + " original contents: " + aryToString(arr));
+
   }
 }
