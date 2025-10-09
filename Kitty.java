71fafb2da953a739b8ed9895a6aabf5578b594e3
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-09 01:04:53 -0400

    Completed constructors, accessors, and modifiers

--- Kitty.java
+++ Kitty.java
@@ -5,4 +5,22 @@ public class Kitty {
         age = setAge;
         name = setName;
     }
+    public Kitty(){
+        this("Mittens", 2);
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void makeOlder(){
+        age++;
+    }
+    public void changeName(String newName){
+        name = newName;
+    }
+    public void changeAge(int newAge){
+        age = newAge;
+    }
 }
