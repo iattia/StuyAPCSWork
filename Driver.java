57161b287a7736710f8d6b3c84f39d6a664d422d
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-09 01:00:58 -0400

    Uncommented out main method

--- Driver.java
+++ Driver.java
@@ -10,34 +10,33 @@ public class Driver{
     
     //2nd constructor makes a Kitty named Mittens that is 2 years old
     //this constructor takes no parameters.
-    /*
+
     Kitty b = new Kitty();
     System.out.println(b);
-    */
+
     //should print The address. Later it will print "The amazing Mittens"
     
     //1. Accessor Methods
-    /*
+
     System.out.println(a.getName() + " is "+a.getAge() + " years old.");
     //should print "Matcha is 3 years old."
-    */
     
     //2. Mutator Method
-    /*
+
     a.makeOlder();
     System.out.println(a.getName() + " is "+a.getAge() + " years old.");
     //should print "Matcha is 4 years old."
-    */
+
     
     //4. Mutator Method
-    /*
+
     a.makeOlder();
     a.changeName("Katya");
     b.changeName("Aytak");
     b.makeOlder();
     System.out.println(a.getName() + " is "+a.getAge() + " years old.");
     System.out.println(b.getName() + " is " + b.getAge() + " years old.");
-    */
+
     //should print "Katya is 5 years old."
     //should print "Aytak is 3 years old."
         
