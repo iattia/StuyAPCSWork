77ae074da43a095cebd2762755f8c257697b5d66
Ibrahim Attia <iattia70@stuy.edu>
2025-10-16 10:49:13 -0400

    Fixed logic for first triangle constructor

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,15 @@
 public class Triangle{
   //Instance Variables
   private double x0, y0, x1, y1, x2, y2;
+  private Point v0, v1, v2;
   //Constructors
   public Triangle(double x0, double y0, double x1, double y1, double x2, double y2){
-    Point p0 = new Point(x0, y0);
-    Point p1 = new Point(x1, y1);
-    Point p2 = new Point(x2, y2);
+    this.v0 = new Point(x0, y0);
+    this.v1 = new Point(x1, y1);
+    this.v2 = new Point(x2, y2);
+  }
+  public Triangle(Point v0,Point v1,Point v2){
+    
   }
   //Methods
 
