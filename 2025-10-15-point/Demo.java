334c424ab40e1f3db75c75f7edb242eff0d2320f
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-15 22:49:52 -0400

    Completed static Demo distance method

--- 2025-10-15-point/Demo.java
+++ 2025-10-15-point/Demo.java
@@ -7,6 +7,9 @@ public class Demo{
   public static double distance(Point a, Point b){
     double x1 = a.getX();
     double y1 = a.getY();
+    double x2 = b.getX();
+    double y2 = b.getY();
+    return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1, 2));
   }
 
   public static void main(String[]args){
