772fbe8f1a017710e74f0fbc454eaddf690e6a17
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-15 23:10:25 -0400

    Created equilateral triangle with side lengths of 1 and printed its 3 side lengths using distance methods

--- 2025-10-15-point/Demo.java
+++ 2025-10-15-point/Demo.java
@@ -17,10 +17,21 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p1);
+    Point p5 = new Point(0,3);
+    Point p6 = new Point(4, 0);
+    Point eq1 = new Point(0,0);
+    Point eq2 = new Point(1,0);
+    Point eq3 = new Point(0.5, Math.sqrt(3)/2);
     System.out.println(p4);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+    System.out.println("Expected 5.0, got: " + distance(p5,p6));
+    System.out.println("Expected 5.0, got: " + Point.distance(p5,p6));
+    System.out.println("Expected 5.0, got: " + p5.distanceTo(p6));
+    System.out.println("Equilateral triangle side 1 (expecting length of 1): " + eq1.distanceTo(eq2));
+    System.out.println("Equilateral triangle side 2 (expecting length of 1): " + eq1.distanceTo(eq3));
+    System.out.println("Equilateral triangle side 3 (expecting length of 1): " + eq2.distanceTo(eq3));
   }
 }
