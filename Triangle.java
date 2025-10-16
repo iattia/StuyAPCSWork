fedba0213b0c76c6ff404dc9223f7a610242d0a0
Ibrahim Attia <iattia70@stuy.edu>
2025-10-16 10:46:46 -0400

    Initialized variables and created first triangle constructor

--- Triangle.java
+++ Triangle.java
@@ -1,8 +1,12 @@
 public class Triangle{
   //Instance Variables
-  
+  private double x0, y0, x1, y1, x2, y2;
   //Constructors
-
+  public Triangle(double x0, double y0, double x1, double y1, double x2, double y2){
+    Point p0 = new Point(x0, y0);
+    Point p1 = new Point(x1, y1);
+    Point p2 = new Point(x2, y2);
+  }
   //Methods
-  
+
 }
