6cb5faa6d31acf984d8a1fa192a38d74f32aba98
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-16 23:37:36 -0400

    Wrote test case for Triangle constructor using points, testing toString and getPerimeter again

--- Driver.java
+++ Driver.java
@@ -16,5 +16,12 @@ public class Driver{
     Triangle t1 = new Triangle(0, 0, 3, 0, 0, 4);
     System.out.println("Triangle t1: " + t1 + " (Expected: {(0.0, 0.0), (3.0, 0.0), (0.0, 4.0)})");
     System.out.println("Perimeter of t1: " + t1.getPerimeter() + " (Expected: 12.0)");
+
+    Point a = new Point(1, 1);
+    Point b = new Point(4, 1);
+    Point c = new Point(1, 5);
+    Triangle t2 = new Triangle(a, b, c);
+    System.out.println("Triangle t2: " + t2 + " (Expected: {(1.0, 1.0), (4.0, 1.0), (1.0, 5.0)})");
+    System.out.println("Perimeter of t2: " + t2.getPerimeter() + " (Expected: 12.0)");
   }
 }
