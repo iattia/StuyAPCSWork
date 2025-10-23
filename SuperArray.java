1db061ecf086a1d95eef4bb0e5b3ac705fe426c2
Ibrahim Attia <iattia70@stuy.edu>
2025-10-23 10:45:01 -0400

    Completed Phase1

--- SuperArray.java
+++ SuperArray.java
@@ -1,3 +1,30 @@
 public class SuperArray{
-
+  private int size, length;
+  private String[] data = new String[];
+  public SuperArray(){
+    length = 10;
+    data = new String[length];
+    size = 0;
+  }
+  public int size(){
+    return this.size;
+  }
+  public boolean add(String value){
+    if (this.size<this.length){
+      this[size+1] = value;
+      return true;
+    }
+    return false;
+  }
+  public String toString(){
+    String arrString = "[";
+    if (this.size == 0){
+      return "[]";
+    }
+    for (int i=0;i<this.size;i++){
+      arrString += this[i] + ", ";
+    }
+    arrString = arrString.substring(0,arrString.length()-2);
+    return arrString
+  }
 }
