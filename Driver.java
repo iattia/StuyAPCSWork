41cc802f5830373a170bd3eb0e8dc14f31a56282
Ibrahim Attia <iattia70@stuy.edu>
2025-10-23 10:58:02 -0400

    Added test cases

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+  public static void main(String[] args){
+    SuperArray sup = new SuperArray();
+    System.out.println(sup);
+    sup.add("1");
+    System.out.println(sup);
+  }
+}
