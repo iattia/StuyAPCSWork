5557b7303c600fced6c5d674533ccf6641c2805c
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-23 22:59:07 -0400

    Completed Phase 3 and Phase 3 testing

--- SuperArray.java
+++ SuperArray.java
@@ -9,13 +9,13 @@ public class SuperArray{
   public int size(){
     return size;
   }
-  public boolean add(String value){
-    if (size<length){
-      data[size] = value;
-      size++;
-      return true;
+  public boolean add(String value) {
+    if (size >= data.length) {
+      resize();
     }
-    return false;
+    data[size] = value;
+    size++;
+    return true;
   }
   public String toString(){
     String arrString = "[";
@@ -44,5 +44,33 @@ public class SuperArray{
     String replaced = data[index];
     data[index] = newVal;
     return replaced;
+  }
+  /* Phase 3 */
+  public SuperArray(int initialCapacity){
+    if (initialCapacity < 0){
+      System.out.println("Error");
+      initialCapacity = 10;
+    }
+    data = new String[initialCapacity];
+    size = 0;
+  }
+  private void resize() {
+    int newLength = 2 * data.length + 1;
+    String[] newData = new String[newLength];
+    for (int i = 0; i < size; i++) {
+      newData[i] = data[i];
+    }
+    data = newData;
+  }
+
+  /* Test for private methods (resize) */
+  public static void main(String[] args) {
+    SuperArray sup = new SuperArray();
+    for (int i = 1; i <= 15; i++) {
+      sup.add("Item" + i);
+    }
+    System.out.println(sup);
+    System.out.println("Size: " + sup.size());
+    System.out.println("Internal array length: " + sup.data.length);
   }
 }
