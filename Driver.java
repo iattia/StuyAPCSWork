7293226626bc9c2d3da676c85842560a423d810f
Ibrahim Attia <hima.attia.123@gmail.com>
2025-10-23 22:59:27 -0400

    Completed Phase 2 testing

--- Driver.java
+++ Driver.java
@@ -4,5 +4,27 @@ public class Driver{
     System.out.println(sup);
     sup.add("1");
     System.out.println(sup);
+    System.out.println("Expected 1, got: " + sup.get(0));
+    System.out.println(sup.set(0,"3"));
+    System.out.println(sup);
+    sup.add("2");
+
+
+    /* Phase 2 Testing */
+    for (int i=0; i<sup.size(); i++){
+      System.out.println(sup.get(i));
+    }
+    for (int i=0; i<sup.size(); i++){
+      sup.set(i,sup.get(i)+"offset");
+    }
+    System.out.println(sup);
+
+    for (int i=sup.size()-1; i>0; i--){
+      sup.set(i-1,sup.get(i));
+      if (i == sup.size()-1){
+        sup.set(i,"NOPE!");
+      }
+    }
+    System.out.println(sup);
   }
 }
