25c9ecb85cede5c4301767542b09b9d706ab39ee
Ibrahim Attia <iattia70@stuy.edu>
2025-11-10 11:01:36 -0500

    Driver from class

--- Driver.java
+++ Driver.java
@@ -1,9 +1,11 @@
 public class Driver{
   public static void main(String[] args){
-    RealNumber rn1 = new RealNumber(10.43);
-    RealNumber rn2 = new RealNumber(3.45);
-    System.out.println(rn1.equals(rn2));
-    System.out.println(rn1.compareTo(rn2));
-    System.out.println(rn1.subtract(rn2));
+    RealNumber re1 = new RealNumber(10.43);
+    RealNumber re2 = new RealNumber(3.45);
+    RationalNumber ra1 = new RationalNumber(3,4);
+    System.out.println(re1.equals(re2));
+    System.out.println(re1.compareTo(re2));
+    System.out.println(re1.subtract(re2));
+    System.out.println(ra1.reciprocal());
   }
 }
