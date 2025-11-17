d46cf92c9a1b74ae7eb652c6fce63e0bfc8a40d9
Ibrahim Attia <iattia70@stuy.edu>
2025-11-17 10:59:17 -0500

    Progress from classwork, completed NoNullArrayList, but getting compilation errors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(int capacity){
+    super.ArrayList(capacity);
+  }
+  public void add(int index, T value){
+    this.add(value);
+  }
+  public boolean add(T value){
+    if (value != null){
+      throw (IllegalArgumentException);
+    } else{
+      this.add(whereToPlace(), value);
+      return true;
+    }
+  }
+  public int whereToPlace(T value){
+    for (int i=0;i<this.size();i++){
+      if (value.compareTo(this.getValue(i))>0){
+        return i+1;
+      }
+    }
+  }
+}
