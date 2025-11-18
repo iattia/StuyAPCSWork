9a8e92176fad8cfa3c563b04559135f421969e21
Ibrahim Attia <iattia70@stuy.edu>
2025-11-18 10:41:26 -0500

    Initial commit

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,23 @@
-
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(int capacity){
+    super(capacity);
+  }
+  public OrderedArrayList(){
+    super();
+  }
+  public int whereToPlace(T value){
+    if (value == null){
+      return 0;
+    }
+    for (int i=0;i<this.size();i++){
+      int comparison = value.compareTo(this.get(i));
+      if (i == 0 && comparison < 0){
+        return 0;
+      }
+      if (comparison>0){
+        return i+1;
+      }
+    }
+    return 0;
+  }
+}
