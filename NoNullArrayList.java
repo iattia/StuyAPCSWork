09ae7daff701a05687828156e959add5f4f24c7b
Ibrahim Attia <iattia70@stuy.edu>
2025-11-18 10:41:12 -0500

    Removed ordering in nonull, added back to orderedarray

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,24 +2,27 @@ import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(int capacity){
-    super.ArrayList(capacity);
+    super(capacity);
+  }
+  public NoNullArrayList(){
+    super();
   }
   public void add(int index, T value){
     this.add(value);
   }
   public boolean add(T value){
     if (value != null){
-      throw (IllegalArgumentException);
+      throw new IllegalArgumentException("Cannot add null!");
     } else{
-      this.add(whereToPlace(), value);
+      this.add(this.whereToPlace(value), value);
       return true;
     }
   }
   public int whereToPlace(T value){
-    for (int i=0;i<this.size();i++){
-      if (value.compareTo(this.getValue(i))>0){
-        return i+1;
-      }
+    if (value == null){
+      return 0;
+    } else{
+      return this.size()-1;
     }
   }
 }
