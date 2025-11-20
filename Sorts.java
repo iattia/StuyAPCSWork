080728866b15aed14a1243881f7e1a7f27011c2f
Ibrahim Attia <iattia70@stuy.edu>
2025-11-20 10:56:14 -0500

    Initial commit of bubblesort

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,23 @@
 public class Sorts{
   public static void bubblesort(int[]data){
-  
+    boolean hasSorted = true;
+    boolean didSort = false;
+    while (hasSorted){
+      for (int i=0;i<data.length-1-i;i++){
+        if (data[i]>(data[i+1])){
+          int temp = data[i];
+          data[i] = data[i+1];
+          data[i+1] = temp;
+          didSort = true;
+        }
+      }
+      hasSorted = didSort;
+    }
   }
 
   /*Do not implement until directions are posted*/
   public static void selectionsort(int[]data){}
-  
+
   /*Do not implement until directions are posted*/
   public static void insertionsort(int[]data){}
 }
