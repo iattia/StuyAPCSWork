56837f0e1c792aa278fb40673d1ba09833f25435
Ibrahim Attia <iattia70@stuy.edu>
2025-11-20 11:01:14 -0500

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Driver{
+  public static void main(String[] args){
+    int[] arr1 = new int[]{0,1,2,3,6,7,4,3};
+    Sorts.bubblesort(arr1);
+    System.out.println(arr1.toString());
+  }
+}
