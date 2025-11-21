7331e5ef721f4203650085be1375f37f1539569b
Ibrahim Attia <hima.attia.123@gmail.com>
2025-11-21 01:06:22 -0500

    Fixed print statement

--- Driver.java
+++ Driver.java
@@ -4,6 +4,6 @@ public class Driver{
   public static void main(String[] args){
     int[] arr1 = new int[]{0,1,2,3,6,7,4,3};
     Sorts.bubblesort(arr1);
-    System.out.println(arr1.toString());
+    System.out.println(Arrays.toString(arr1));
   }
 }
