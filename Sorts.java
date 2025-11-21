397481046674ef04a340488bbc9a026a90d1fe2c
Ibrahim Attia <iattia70@stuy.edu>
2025-11-21 09:42:42 -0500

    Fixed number of iterations in bubblesort

--- Sorts.java
+++ Sorts.java
@@ -1,13 +1,19 @@
 public class Sorts{
   public static void bubblesort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = 0; j < arr.length - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-          int temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+    boolean shouldContinue = true;
+    int iterator = arr.length - 1;
+    while (shouldContinue) {
+      boolean didSwap = false;
+      for (int i = 0; i < iterator; i++) {
+        if (arr[i] > arr[i + 1]) {
+          int temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          didSwap = true;
         }
       }
+      iterator --;
+      shouldContinue = didSwap;
     }
   }
 
