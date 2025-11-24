db68bd979ab05300e072da3bf494f555b2770969
Ibrahim Attia <iattia70@stuy.edu>
2025-11-24 10:59:40 -0500

    tested selection sort

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Driver{
   public static void main(String[] args){
     int[] arr1 = new int[]{0,1,2,3,6,7,4,3};
-    Sorts.bubblesort(arr1);
+    Sorts.selectionsort(arr1);
     System.out.println(Arrays.toString(arr1));
   }
 }
