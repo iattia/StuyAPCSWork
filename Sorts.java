0d37bdc8617327c526f0f0b78ca5d5d0c4ed4af9
Ibrahim Attia <iattia70@stuy.edu>
2025-11-26 10:57:10 -0500

    Answered questions regarding times

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,9 @@
+// Time for each doubling of the list size from 10k to 20k to 40k: selection increases by 0.365s and then 1.227s, bubble increases by 0.381s and then 1.639s, and insertion increases by 0.075s and then 0.269s. As for the time curves, insertion times follow a quadratic trend: n^2, but they are fastest; bubble times also follow a quadratic trend: n^2, but are much slower; selection times follow a similar trend as bubble times: n^2, but somewhere in between insertion and bubble for actual speeds.
+/*
+selection: 10k: 0.223s 20k: 0.588s 40k: 1.815s 80k: 5.263s
+bubble: 10k: 0.208s 20k: 0.589s 40k: 2.228s 80k: 8.773s
+insertion: 10k: 0.099s 20k: 0.174s 40k: 0.443s 80k: 1.660s
+*/
 public class Sorts{
   public static void bubblesort(int[] arr) {
     boolean shouldContinue = true;
