baa982cfbd525d4f275b24336d1448357a4543f9
Ibrahim Attia <iattia70@stuy.edu>
2025-12-02 11:01:55 -0500

    Began flag

--- 2025-12-02-colors/ColorDemo.java
+++ 2025-12-02-colors/ColorDemo.java
@@ -5,9 +5,14 @@ public class ColorDemo {
   public static final String YELLOW_BACKGROUND = "\u001B[43m";
   public static final String RED_BACKGROUND = "\u001B[41m";
   public static final String RESET = "\u001b[0m";
-  public static final String EMPTY_SPACE = "          ";
+  public static final String EMPTY_SPACE = "                       ";
   public static void main(String[] args){
-    System.out.print(BLACK_BACKGROUND+EMPTY_SPACE+WHITE_BACKGROUND+EMPTY_SPACE+CYAN_BACKGROUND+EMPTY_SPACE);
+    for (int i=0;i<2;i++){
+      System.out.println(RED_BACKGROUND+EMPTY_SPACE+EMPTY_SPACE+EMPTY_SPACE);
+    }
+    for (int i=0; i<2;i++){
+      System.out.println(WHITE_BACKGROUND+EMPTY_SPACE+YELLOW_BACKGROUND+EMPTY_SPACE+WHITE_BACKGROUND+EMPTY_SPACE);
+    }
     System.out.println(RESET);
   }
 }
