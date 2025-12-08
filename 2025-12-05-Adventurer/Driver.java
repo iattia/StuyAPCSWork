b35b9b61efd6f1e927d209c0f07e545f461cdf71
Ibrahim Attia <iattia70@stuy.edu>
2025-12-08 11:00:57 -0500

    Worked on interactive part of the game, done with player turn, still needs to complete opponent turn

--- 2025-12-05-Adventurer/Driver.java
+++ 2025-12-05-Adventurer/Driver.java
@@ -1,44 +1,38 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
+      Scanner userInput = new Scanner(System.in);
+      System.out.print("Enter username: ");
+      String userName = userInput.nextLine();
+      Adventurer player = new Witch(userName);
+      Adventurer opponent = new CodeWarrior("Warrior");
+      while (true) {
+        if (player.getHP() < 0){
+          System.out.println(opponent + " wins!");
+          break;
+        }
+        if (opponent.getHP() < 0){
+          System.out.println(player.getName() + "wins!");
+          break;
+        }
+        System.out.println(player);
+        System.out.println(opponent+" (" + opponent.getClass().getName() + ") - HP: " + opponent.getHP() + "/"+opponent.getmaxHP()+ " | "+ opponent.getSpecialName()+": "+opponent.getSpecial()+"/"+opponent.getSpecialMax());
+        System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        String selection = userInput.nextLine();
+        if (selection.toLowerCase().equals("a") || selection.toLowerCase().equals("attack")){
+          player.attack(opponent);
+        } else if (selection.toLowerCase().equals("sp") || selection.toLowerCase().equals("special")){
+          player.specialAttack(opponent);
+        } else if (selection.toLowerCase().equals("su") || selection.toLowerCase().equals("support")){
+          player.support();
+        } else if (selection.toLowerCase().equals("quit")){
+          break;
+        } else {
+          System.out.println("Please check for typos and try again.");
+          continue;
+        }
 
-        Adventurer w1 = new Witch("Harry");
-        Adventurer w2 = new Witch("Selene", 60, 80);
-
-        System.out.println("=== Initial Stats ===");
-        System.out.println(w1);
-        System.out.println(w2);
-        System.out.println();
-
-        System.out.println("=== Harry attacks Selene ===");
-        System.out.println(w1.attack(w2));
-        System.out.println(w2);
-        System.out.println();
-
-        System.out.println("=== Selene heals Harry ===");
-        System.out.println(w2.support(w1));
-        System.out.println(w1);
-        System.out.println();
-
-        System.out.println("=== Harry heals herself ===");
-        System.out.println(w1.support());
-        System.out.println(w1);
-        System.out.println();
-
-        System.out.println("=== Selene uses special attack on Harry ===");
-        System.out.println(w2.specialAttack(w1));
-        System.out.println(w2);
-        System.out.println(w1);
-        System.out.println();
-
-        System.out.println("=== Draining Selene’s mana ===");
-        w2.setSpecial(10);
-        System.out.println(w2.specialAttack(w1)); // should fail
-        System.out.println(w2);
-        System.out.println();
-
-        System.out.println("=== Restoring Selene’s mana ===");
-        int restored = w2.restoreSpecial(50);
-        System.out.println("Mana restored: " + restored);
-        System.out.println(w2);
+      }
     }
 }
