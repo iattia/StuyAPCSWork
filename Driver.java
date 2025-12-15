7d9dc2a57ce979e2fba9bfc9ec8521ce111d4dc8
Ibrahim Attia <iattia70@stuy.edu>
2025-12-15 11:01:36 -0500

    Started Driver

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+  public static void main(String[] args){
+    MyLinkedList ll1 = new MyLinkedList();
+    System.out.println(ll1);
+    ll1.add("e");
+    ll1.add("f");
+    System.out.println(ll1);
+  }
+}
