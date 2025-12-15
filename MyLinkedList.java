809435621fa6b988622f868fffcdd966ae4a7bc4
Ibrahim Attia <iattia70@stuy.edu>
2025-12-15 10:45:37 -0500

    Completed constructor and toString

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,45 +4,58 @@ public class MyLinkedList{
 
   //You will only write the no argument Constructor, as you do not have a capacity.
   public MyLinkedList(){
+    size = 0
+    front = null;
+    back = null;
   }
-  
+
   public int size(){
     //Returns the number of elements in this list.
+    return size;
   }
   public String get(int index){
     //Return the value at the specified index.
   }
-  
+
   public boolean add(String e){
     //Appends the specified element to the end of this list.
+    new ListNode
     return true;
   }
 
   public String toString(){
     //same format as ArrayList "[A, B, C]" or "[]"
-    return "";
+    String listString = "[";
+    for (int i=0;i<size;i++){
+      listString += this.get(i);
+      if (i!= size-1){
+        listString += ", ";
+      }
+    }
+    listString += "]";
+    return listString;
   }
 
    public void add(int index, String element){
     //Inserts the specified element at the specified position in this list.
   }
-  
+
    public String set(int index, String value){
     //Replaces the value at the specified index in this list with the specified value.
     //Return the original String that was present
-  } 
-  
+  }
+
   public boolean contains(String element){
     //Returns true if this list contains the specified element, false otherwise.
   }
-  
+
   public String remove(int index){
     //Removes the element at the specified position in this list..
   }
-  
+
   public boolean remove(String element){
     //Removes the first occurrence of the specified element from this list, if it is present..
   }
-  
+
 
 }
