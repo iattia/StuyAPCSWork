9fe5c0b16d039150a5e37b3b57ee971fc4fb8046
Ibrahim Attia <iattia70@stuy.edu>
2025-12-15 10:38:17 -0500

    Completed ListNode

--- ListNode.java
+++ ListNode.java
@@ -3,20 +3,39 @@ public class ListNode{
   private ListNode next,prev;
 
   public ListNode(String d){ //default next/prev should be null
+    next = null;
+    prev = null;
+    data = d;
   }
-  
-  public String toString(){} //Return the string of the data (matters more for other types of nodes)
-  
-  public String getData(){} //return the data
-  
-  public ListNode next(){} //return the next node
-  
-  public ListNode prev(){} //return the previous node
-  
-  public String setData(String newdata){}//replace the data, with the newdata, return the original data.
-  
-  public void setNext(ListNode n){}
-  
-  public void setPrev(ListNode n){} 
+
+  public String toString(){
+    return data;
+  } //Return the string of the data (matters more for other types of nodes)
+
+  public String getData(){
+    return data;
+  } //return the data
+
+  public ListNode next(){
+    return next;
+  } //return the next node
+
+  public ListNode prev(){
+    return prev;
+  } //return the previous node
+
+  public String setData(String newdata){
+    String originalData = data;
+    data = newdata;
+    return originalData;
+  }//replace the data, with the newdata, return the original data.
+
+  public void setNext(ListNode n){
+    next = n;
+  }
+
+  public void setPrev(ListNode n){
+    prev = n;
+  }
 
 }
