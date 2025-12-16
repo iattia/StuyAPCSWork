55310d5dbc6579413de038a1f0343a2f0678349e
Ibrahim Attia <iattia70@stuy.edu>
2025-12-16 11:01:30 -0500

    Made small changes for minor tests

--- Driver.java
+++ Driver.java
@@ -4,6 +4,9 @@ public class Driver{
     System.out.println(ll1);
     ll1.add("e");
     ll1.add("f");
+    ll1.add("d");
+    ll1.add("d");
+    ll1.add(2,"2");
     System.out.println(ll1);
   }
 }
