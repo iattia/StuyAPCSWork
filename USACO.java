19f365bbbaec5a34645508610ea697d8bc72db8d
Ibrahim Attia <iattia70@stuy.edu>
2026-01-30 11:30:26 -0500

    Completed stomp method

--- USACO.java
+++ USACO.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class USACO{
   public static void main(String[] args) throws FileNotFoundException{
+    int totalVolume = 0;
     File file = new File("makelake.0.in");
     Scanner scanner = new Scanner(file);
     int R = scanner.nextInt();
@@ -15,12 +16,34 @@ public class USACO{
         grid[r][c] = scanner.nextInt();
       }
     }
-    scanner.close();
     System.out.println(Arrays.deepToString(grid));
-
+    for (int n=0; n<N;n++){
+      int R_s = scanner.nextInt()-1;
+      int C_s = scanner.nextInt()-1;
+      int D_s = scanner.nextInt();
+      stomp(grid, R_s, C_s, D_s);
+    }
+    System.out.println(Arrays.deepToString(grid));
   }
-  //public int[][] getData(String filename)
 
+  public static void stomp(int[][]grid, int rows, int cols, int depth){
+    int currentMax = 0;
+    for (int i = 0; i<rows;i++){
+      for (int j = 0;j<cols;j++){
+        if (grid[i][j]>currentMax){
+          currentMax = grid[i][j];
+        }
+      }
+    }
+    int newElev = currentMax-depth;
+    for (int i = 0; i<rows;i++){
+      for (int j = 0;j<cols;j++){
+        if (grid[i][j]>newElev){
+          grid[i][j] = newElev;
+        }
+      }
+    }
+  }
   public static long bronze(String filename){
     return 0;
   }
