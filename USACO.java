6c5a262995b5863b815f373993f702ce46682d26
Ibrahim Attia <iattia70@stuy.edu>
2026-01-30 11:45:21 -0500

    resolved formatting for bronze

--- USACO.java
+++ USACO.java
@@ -2,39 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class USACO{
-  public static void main(String[] args) throws FileNotFoundException{
-    int totalVolume = 0;
-    File file = new File("makelake.0.in");
-    Scanner scanner = new Scanner(file);
-    int R = scanner.nextInt();
-    int C = scanner.nextInt();
-    int E = scanner.nextInt();
-    int N = scanner.nextInt();
-    int[][] grid = new int[R][C];
-    for (int r=0; r<R;r++){
-      for (int c=0;c<C;c++){
-        grid[r][c] = scanner.nextInt();
-      }
-    }
-    System.out.println(Arrays.deepToString(grid));
-    for (int n=0; n<N;n++){
-      int R_s = scanner.nextInt()-1;
-      int C_s = scanner.nextInt()-1;
-      int D_s = scanner.nextInt();
-      stomp(grid, R_s, C_s, D_s);
-    }
-    System.out.println(Arrays.deepToString(grid));
-    for (int r=0; r<R;r++){
-      for (int c=0;c<C;c++){
-        if (E-grid[r][c]>0){
-          totalVolume += E-grid[r][c];
-        }
-      }
-    }
-    totalVolume = totalVolume *72*72;
-    System.out.println(totalVolume);
-  }
-
   public static void stomp(int[][]grid, int rows, int cols, int depth){
     int currentMax = 0;
     for (int i = 0; i<3;i++){
@@ -53,8 +20,35 @@ public class USACO{
       }
     }
   }
-  public static long bronze(String filename){
-    return 0;
+  public static long bronze(String filename) throws FileNotFoundException{
+    long totalVolume = 0;
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+    int R = scanner.nextInt();
+    int C = scanner.nextInt();
+    int E = scanner.nextInt();
+    int N = scanner.nextInt();
+    int[][] grid = new int[R][C];
+    for (int r=0; r<R;r++){
+      for (int c=0;c<C;c++){
+        grid[r][c] = scanner.nextInt();
+      }
+    }
+    for (int n=0; n<N;n++){
+      int R_s = scanner.nextInt()-1;
+      int C_s = scanner.nextInt()-1;
+      int D_s = scanner.nextInt();
+      stomp(grid, R_s, C_s, D_s);
+    }
+    for (int r=0; r<R;r++){
+      for (int c=0;c<C;c++){
+        if (E-grid[r][c]>0){
+          totalVolume += E-grid[r][c];
+        }
+      }
+    }
+    totalVolume = totalVolume *72*72;
+    return totalVolume;
   }
 
   public static long silver(String filename){
