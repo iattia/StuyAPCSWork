5c65314c8279f908506b423d9401ca331ff8b5eb
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-04 01:52:39 -0500

    Completed groupSum

--- 2026-02-03-recursion/Backtracking.java
+++ 2026-02-03-recursion/Backtracking.java
@@ -16,5 +16,17 @@ public class Backtracking {
     }
     return count;
   }
-}
 
+  public static boolean groupSum(int start, int[] nums, int target) {
+    if (start == nums.length) {
+      return target == 0;
+    }
+    if (groupSum(start + 1, nums, target - nums[start])) {
+      return true;
+    }
+    if (groupSum(start + 1, nums, target)) {
+      return true;
+    }
+    return false;
+  }
+}
