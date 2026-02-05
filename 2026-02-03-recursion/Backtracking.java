a8db832217021e202a3f6705b36c752fd37010cc
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-05 02:03:33 -0500

    Completed groupSum5

--- 2026-02-03-recursion/Backtracking.java
+++ 2026-02-03-recursion/Backtracking.java
@@ -83,4 +83,17 @@ public class Backtracking {
     }
     return split53Helper(index + 1, nums, sum1 + current, sum2) || split53Helper(index + 1, nums, sum1, sum2 + current);
   }
+
+  public static boolean groupSum5(int start, int[] nums, int target) {
+    if (start >= nums.length) {
+      return target == 0;
+    }
+    if (nums[start] % 5 == 0) {
+      if (start + 1 < nums.length && nums[start + 1] == 1) {
+        return groupSum5(start + 2, nums, target - nums[start]);
+      }
+      return groupSum5(start + 1, nums, target - nums[start]);
+    }
+    return groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target);
+  }
 }
