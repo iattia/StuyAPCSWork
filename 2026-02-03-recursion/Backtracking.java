26e7c85decb953533f0635ea20245946e99a797d
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-05 02:40:32 -0500

    Completed groupSumClump

--- 2026-02-03-recursion/Backtracking.java
+++ 2026-02-03-recursion/Backtracking.java
@@ -96,4 +96,20 @@ public class Backtracking {
     }
     return groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target);
   }
+
+  public static boolean groupSumClump(int start, int[] nums, int target) {
+    if (start >= nums.length) {
+      return target == 0;
+    }
+    int i = start;
+    int sum = 0;
+    while (i < nums.length && nums[i] == nums[start]) {
+      sum += nums[i];
+      i++;
+    }
+    if (groupSumClump(i, nums, target - sum)) {
+      return true;
+    }
+    return groupSumClump(i, nums, target);
+  }
 }
