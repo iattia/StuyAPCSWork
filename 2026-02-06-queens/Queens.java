f91c0b6d1e83f0ee695e76a98f48d75156708b36
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-08 22:52:54 -0500

    Completed modifyBoard method

--- 2026-02-06-queens/Queens.java
+++ 2026-02-06-queens/Queens.java
@@ -1,19 +1,29 @@
 public class Queens{
   private static boolean addQueen(int r, int c, int[][] board){
-
+    
   }
 
   private static void removeQueen(int r, int c, int[][] board){
 
   }
 
-  private static void modifyBoard(int r, int c, int[][] board, boolean add?){
-    // c-- until c=1 or r=n and c++ until c=8 or r=n
-    if (add){
-      while (r < board.length){
-        board[r][c] = -1;
-        board[r+1][c-1] += 1;
-        board[r+1][c+1] += 1;
+  private static void modifyBoard(int r, int c, int[][] board, boolean isAdding){
+    int modifier;
+    if (isAdding){
+      modifier = 1;
+      board[r][c] = -1;
+    } else{
+      modifier = -1;
+      board[r][c] = 0;
+    }
+    for (int i = 1; i < board.length; i++){
+      if (r + i < board.length){
+        if (c - i >= 0){
+          board[r + i][c - i] += modifier;
+        }
+        if (c + i < board.length){
+          board[r + i][c + i] += modifier;
+        }
       }
     }
   }
