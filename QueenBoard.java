36fbc20d98118cf13cee086c33a17be93d9dbbb5
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-11 03:28:04 -0500

    Fixed clear method and also removed a lot of redundant code in both toStrings

--- QueenBoard.java
+++ QueenBoard.java
@@ -13,17 +13,17 @@ public class QueenBoard {
   }
 
   /**
-   * @return The output string formatted as follows:
-   *         All numbers that represent queens are replaced with 'Q'
-   *         all others are displayed as underscores '_'
-   *         There are spaces between each symbol:
-   *         _ _ Q _
-   *         Q _ _ _
-   *         _ _ _ Q
-   *         _ Q _ _
-   *         excludes the indentation, when in string form should be exactly:
-   *         "_ _ Q _\nQ _ _ _\n_ _ _ Q\n_ Q _ _"
-   */
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  _ _ Q _
+  Q _ _ _
+  _ _ _ Q
+  _ Q _ _
+  *excludes the indentation, when in string form should be exactly:
+  * "_ _ Q _\nQ _ _ _\n_ _ _ Q\n_ Q _ _"
+  */
   public String toString() {
     String boardString = "";
     for (int r = 0; r < board.length; r++) {
@@ -68,14 +68,12 @@ public class QueenBoard {
   }
 
   /**
-   * @return true when the queen added correctly, false Otherwise
-   * @precondition r and c are valid indices of the instance variable board
-   * @postcondition the board is only changed when the function returns true
-   *                in which case the queen is added and all it's threatened
-   *                positions are incremented.
-   *                Only the squares in rows that are higher index than r should
-   *                be marked as threatened.
-   */
+  *@return true when the queen added correctly, false Otherwise
+  *@precondition r and c are valid indices of the instance variable board
+  *@postcondition the board is only changed when the function returns true
+  * in which case the queen is added and all it's threatened positions are incremented.
+  * Only the squares in rows that are higher index than r should be marked as threatened.
+  */
   private boolean addQueen(int r, int c) {
     if (board[r][c] != 0) {
       return false;
@@ -85,13 +83,11 @@ public class QueenBoard {
   }
 
   /**
-   * Remove the queen that was added to r,c
-   * 
-   * @precondition r and c are valid indices of the board array and there is a
-   *               queen at position r,c
-   * @postcondition the board is modified to remove that queen and all it's
-   *                threatened positions are decremented
-   */
+  *Remove the queen that was added to r,c
+  *@precondition r and c are valid indices of the board array and there is a queen at position r,c
+  *@postcondition the board is modified to remove that queen and all it's
+  *threatened positions are decremented
+  */
   private void removeQueen(int r, int c) {
     modifyBoard(r, c, board, false);
   }
@@ -124,23 +120,15 @@ public class QueenBoard {
     board = new int[board.length][board.length];
   }
 
-  /**
-   * Find the first solution configuration possible for this size board. Start by
-   * placing
-   * the 1st queen in the top left corner, and each new queen in the next ROW.
-   * When backtracking
-   * move the previous queen to the next valid space. This means everyone will
-   * generate the same
-   * first solution.
-   * 
-   * @precondition: the board is filled with 0's (you don't try to solve an
-   *                already solved board.)
-   * @postcondition: the board remains in a solved state.
-   * @return false when the board is not solveable and leaves the board filled
-   *         with zeros;
-   *         returns true when the board is solveable, and leaves the board in a
-   *         solved state
-   */
+  /**Find the first solution configuration possible for this size board. Start by placing
+  *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
+  *  move the previous queen to the next valid space. This means everyone will generate the same
+  *  first solution.
+  *@precondition: the board is filled with 0's (you don't try to solve an already solved board.)
+  *@postcondition: the board remains in a solved state.
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        returns true when the board is solveable, and leaves the board in a solved state
+  */
   public boolean solve() {
     return solve(0);
   }
@@ -160,13 +148,9 @@ public class QueenBoard {
     return false;
   }
 
-  /**
-   * Find all possible solutions to this size board. You must clear the board
-   * before attempting to solve.
-   * 
-   * @return the number of solutions found, and leaves the board filled with only
-   *         0's
-   */
+  /**Find all possible solutions to this size board. You must clear the board before attempting to solve.
+  *@return the number of solutions found, and leaves the board filled with only 0's
+  */
 
   public int countSolutions() {
     return countSolutions(0);
