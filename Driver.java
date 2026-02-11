3e91a3b0a1e0f75564fd3ccbc1f02a8d7f5c4cc0
Ibrahim Attia <hima.attia.123@gmail.com>
2026-02-11 03:39:47 -0500

    Changed driver to match description in assignment

--- Driver.java
+++ Driver.java
@@ -1,22 +1,28 @@
 public class Driver{
   public static void main(String[]args){
     QueenBoard b;
-    int size = 4;
+    int size = 8;
+    if (args.length > 0) {
+      size = Integer.parseInt(args[0]);
+    }
     try{
-	b = new QueenBoard(size);
-  	b.solve();
-  	System.out.println(b);
+      b = new QueenBoard(size);
+      if (args.length > 1) {
+        b.setAnimate(true);
+        b.setDelay(Integer.parseInt(args[1]));
+      }
+      b.solve();
+      System.out.println(b);
     }catch(Exception e){
-	System.out.println("Fail solve size "+size+" board, exception"+e);
+	    System.out.println("Fail solve size "+size+" board, exception"+e);
     }
 
 
     try{
-	b = new QueenBoard(size);
-  	System.out.println(b.countSolutions());
+      b = new QueenBoard(size);
+      System.out.println(b.countSolutions());
     }catch(Exception e){
-	System.out.println("Fail countsolutions size "+size+" board, exception"+e);
+	    System.out.println("Fail countsolutions size "+size+" board, exception"+e);
     }
-    
   }
 }
