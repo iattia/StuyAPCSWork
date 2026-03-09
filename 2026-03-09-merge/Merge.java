2ebb6019d5d4e79080c54b0a1676dbee0c94f336
Ibrahim Attia <iattia70@stuy.edu>
2026-03-09 11:46:58 -0400

    Initial commit

---
+++ 2026-03-09-merge/Merge.java
@@ -0,0 +1,14 @@
+public class Merge{
+
+  /**Merge two sorted arrays into one sorted array
+  *@param left is a sorted array
+  *@param right is a sorted array
+  *@return a sorted array that contains all of the elements of both left and right combined.
+  */
+  public static int[] merge(int [] left, int[] right){
+
+  }
+  public static void mergesort(int[] data, int[] temp, int lo, int hi){
+
+  }
+}
