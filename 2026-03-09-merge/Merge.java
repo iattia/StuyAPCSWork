0edb29c9b79756622850e1e50656dac15885446d
Ibrahim Attia <74513877+iattia@users.noreply.github.com>
2026-03-10 02:08:33 -0400

    Completed merge

--- 2026-03-09-merge/Merge.java
+++ 2026-03-09-merge/Merge.java
@@ -6,7 +6,32 @@ public class Merge{
   *@return a sorted array that contains all of the elements of both left and right combined.
   */
   public static int[] merge(int [] left, int[] right){
-
+    int[] sorted = new int[left.length + right.length];
+    int l = 0;
+    int r = 0;
+    int i = 0;
+    while (l < left.length && r < right.length) {
+      if (left[l] <= right[r]) {
+        sorted[i] = left[l];
+        i++;
+        l++;
+      } else {
+        sorted[i] = right[r];
+        i++;
+        r++;
+      }
+    }
+    while (l < left.length) {
+      sorted[i] = left[l];
+      i++;
+      l++;
+    }
+    while (r < right.length) {
+      sorted[i] = right[r];
+      i++;
+      r++;
+    }
+    return sorted;
   }
   public static void mergesort(int[] data, int[] temp, int lo, int hi){
 
