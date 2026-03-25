cac6b6247180fd72a9fbbfa03c39c76cc630227e
Ibrahim Attia <iattia70@stuy.edu>
2026-03-25 11:37:09 -0400

    Fixed size and add

--- Frontier.java
+++ Frontier.java
@@ -4,10 +4,10 @@ public class Frontier{
   private ArrayDeque<int[]> frontier;
   public Frontier(){}
   public int size(){
-    return frontier.getSize();
+    return frontier.size();
   }
   public void add(int[]location){
-    frontier.addLast(location)
+    frontier.addLast(location);
   }
   public int[] remove(){
     return frontier.poll();
